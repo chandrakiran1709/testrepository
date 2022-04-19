84a6043d07952d7a7d21d6a5c5c8f7e220cceae1
chandrakiran6178 <chandrakiran6178@gmail.com>
2022-04-19 23:00:07 +0530

    commit

---
+++ Desktop/react- bootstrap/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Banner from './Banner';
+import Cards from './Cards';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+      <Cards/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
